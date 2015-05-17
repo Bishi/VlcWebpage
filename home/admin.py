9f7213772a39f83be912866a88b02c10ab476ffc
Jernej Mrvar <jernej.mrvar@gmail.com>
2015-05-17 22:13:22 +0200

    UserAdmin change

--- home/admin.py
+++ home/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from home.models import NewsArticle, Recruitment, ClassName, ClassRole, WarcraftlogsAPI, WarcraftlogsURL, RealmStatusAPI
 from home.models import ArticleComment
 from home.models import Chatterbox
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext, ugettext_lazy as _
 
 
 class NewsArticleAdmin(admin.ModelAdmin):
@@ -92,3 +95,26 @@ class ArticleCommentAdmin(admin.ModelAdmin):
     list_display = ('id', 'author', 'origin', 'body')
 
 admin.site.register(ArticleComment, ArticleCommentAdmin)
+
+
+#custom user admin, only superusers can change 'is superuser' status
+class MyUserAdmin(UserAdmin):
+    def get_fieldsets(self, request, obj=None):
+        if not obj:
+            return self.add_fieldsets
+
+        if request.user.is_superuser:
+            perm_fields = ('is_active', 'is_staff', 'is_superuser',
+                           'groups', 'user_permissions')
+        else:
+            # modify these to suit the fields you want your
+            # staff user to be able to edit
+            perm_fields = ('is_active', 'is_staff', 'groups')
+
+        return [(None, {'fields': ('username', 'password')}),
+                (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+                (_('Permissions'), {'fields': perm_fields}),
+                (_('Important dates'), {'fields': ('last_login', 'date_joined')})]
+
+admin.site.unregister(User)
+admin.site.register(User, MyUserAdmin)
